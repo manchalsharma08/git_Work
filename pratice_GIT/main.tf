63d66431032c219d8864a1d0b8763fbd3f086847
manchalsharma08 <159640059+manchalsharma08@users.noreply.github.com>
2024-08-26 09:22:23 +0530

    add vnet code

--- pratice_GIT/main.tf
+++ pratice_GIT/main.tf
@@ -11,4 +11,12 @@ account_replication_type = "LRS"
 account_tier = "Standard"
 }
 
+resource "azurerm_virtual_network" "vnet" {
+
+    name = "man-vnet"
+    location = "westus"
+    resource_group_name = "man-rg"
+    address_space = ["10.0.0.0/16"]
+}
+
 
