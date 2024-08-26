0a32491909d6876b61990cbcfbedfbb0d59c0e4f
manchalsharma08 <159640059+manchalsharma08@users.noreply.github.com>
2024-08-26 11:22:44 +0530

    deleted app services

--- pratice_GIT/main.tf
+++ pratice_GIT/main.tf
@@ -11,39 +11,7 @@ account_replication_type = "LRS"
 account_tier = "Standard"
 }
 
-resource "azurerm_app_service_plan" "appplan" {
-name = "appplan-man008"
-location = "westus"
-resource_group_name = "man-rg"
 
-sku {
-tier ="Standard"
-size ="S1"
-}
-}
-
-## Add app Services Block
-resource "azurerm_app_service" "example" {
-  name                = "example-app-service"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
-  }
-
-  connection_string {
-    name  = "Database"
-    type  = "SQLServer"
-    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
-  }
-}
 resource "azurerm_virtual_network" "vnet" {
 
     name = "man-vnet"
