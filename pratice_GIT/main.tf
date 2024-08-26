92235f4982bd5d73260f061f417d13cddc539bb9
manchalsharma08 <159640059+manchalsharma08@users.noreply.github.com>
2024-08-26 08:06:42 +0530

    add Storage account

--- pratice_GIT/main.tf
+++ pratice_GIT/main.tf
@@ -3,4 +3,12 @@ name = "man-rg"
 location = "westus"
 }
 
+resource "azurerm_storage_account" "st" {
+name= "man846586mn"
+location ="westus"
+resourec_group_name = "man-rg"
+account_replication_type = "LRS"
+account_tier = "Standard"
+}
+
 
